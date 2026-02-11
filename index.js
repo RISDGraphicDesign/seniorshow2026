5715a9aba4c010a1059328f8057320a1f6404d12
Maggie Xian <111546244+meitongxian@users.noreply.github.com>
2026-02-11 16:35:04 -0500

    improve mobile resize

--- index.js
+++ index.js
@@ -4,3 +4,15 @@ landing.addEventListener("click", () => {
     landing.remove();
     document.body.innerHTML += '<script src="game.js"></script>';
 });
+
+// resize vh for mobile
+mobileResize();
+
+function mobileResize() {
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty("--vh", `${vh}px`);
+}
+
+window.addEventListener("resize", () => {
+    mobileResize();
+});
